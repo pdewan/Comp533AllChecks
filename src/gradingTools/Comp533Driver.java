73f73df85040f609198887041a91579e5609e0d1
avitkus <avitkus7@gmail.com>
2018-01-26 11:00:48 -0500

    Setup maven
    added pom
    added exit call when in headless mode

--- src/gradingTools/Comp533Driver.java
+++ src/gradingTools/Comp533Driver.java
@@ -3,5 +3,10 @@ package gradingTools;
 public class Comp533Driver {
 	public static void main (String[] args) {
 		Driver.main(args);
+		for(String s : args) {
+			if (s.matches("-.*?[hH][eE][aA][dD][lL][eE][sS]{2}.*")) { // any variety of -.*headless.* with any capitalization
+				System.exit(0);
+			}
+		}
 	}
 }
