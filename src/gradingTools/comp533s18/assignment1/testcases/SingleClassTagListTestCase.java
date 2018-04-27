c89a49a188e34d419afa03a0e51405b19de4a729
pdewan <dewan@cs.unc.edu>
2018-04-27 15:03:05 -0400

    TagList test case

---
+++ src/gradingTools/comp533s18/assignment1/testcases/SingleClassTagListTestCase.java
@@ -0,0 +1,52 @@
+package gradingTools.comp533s18.assignment1.testcases;
+
+import java.util.Arrays;
+import java.util.Set;
+
+import framework.grading.testing.BasicTestCase;
+import grader.basics.junit.NotAutomatableException;
+import grader.basics.junit.TestCaseResult;
+import grader.basics.project.BasicProjectIntrospection;
+import grader.basics.project.ClassDescription;
+import grader.basics.project.NotGradableException;
+import grader.basics.project.Project;
+
+public class SingleClassTagListTestCase extends BasicTestCase {
+	private final String[] tags;
+	protected boolean checkAllTags = true;
+	
+	
+	public SingleClassTagListTestCase(String... aTags) {
+//		super("Prompt printer test case");
+		super("Single class tagged '" + Arrays.toString(aTags) + "' test case");
+		this.tags = aTags;
+	}
+	
+	public SingleClassTagListTestCase(boolean aCheckAllTags, String... aTags) {
+		this(aTags);
+		checkAllTags = aCheckAllTags;
+
+	}
+	
+	
+	@Override
+	public TestCaseResult test(Project project, boolean autoGrade) throws NotAutomatableException,
+			NotGradableException {
+		boolean aCurrentCheckAllTags = BasicProjectIntrospection.isCheckAllSpecifiedTags();
+		if (!checkAllTags) {
+			BasicProjectIntrospection.setCheckAllSpecifiedTags(checkAllTags);
+		}
+		Set<Class> aClasses = BasicProjectIntrospection.findClassesByTag(project, tags);
+		if (!checkAllTags) {
+			BasicProjectIntrospection.setCheckAllSpecifiedTags(aCurrentCheckAllTags);
+		}
+
+    	if (aClasses.size() == 1) {
+    		return pass();
+    	}
+    	if (aClasses.size() > 1) {
+    		return partialPass(0.5, "Multiple classes tagged:" + Arrays.toString(tags) + " " + aClasses);
+    	}
+    	return fail("No class tagged: " + Arrays.toString(tags));
+	}
+}
