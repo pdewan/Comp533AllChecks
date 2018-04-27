c6afd43507dadebcf1c46d7c0fe63c3fa2bee056
pdewan <dewan@cs.unc.edu>
2018-04-27 13:25:37 -0400

    For some reason all asignment requirements have changed

--- src/gradingTools/comp533s18/assignment4/Assignment4Requirements.java
+++ src/gradingTools/comp533s18/assignment4/Assignment4Requirements.java
@@ -1,8 +1,12 @@
 package gradingTools.comp533s18.assignment4;
 
+import util.annotations.Comp533Tags;
+import util.tags.DistributedTags;
+import grader.basics.project.BasicProjectIntrospection;
 import grader.junit.AJUnitProjectRequirements;
 import grader.trace.GraderTraceUtility;
 import grader.trace.comp533.Comp533TraceUtility;
+import gradingTools.comp533s18.assignment1.testcases.SingleClassTagListTestCase;
 import gradingTools.comp533s18.assignment1.testcases.SingleClassTaggedTestCase;
 import gradingTools.comp533s18.assignment3.testcases.MetaStateBroadcastTestCase;
 import gradingTools.comp533s18.assignment3.testcases.OneClientCorrectConnectionTestCase;
@@ -12,18 +16,38 @@ import gradingTools.comp533s18.assignment3.testcases.TwoClientCorrectReadWriteTe
 import gradingTools.comp533s18.assignment4.testcases.TwoClientCorrectConnectionTestCase;
 
 public class Assignment4Requirements extends AJUnitProjectRequirements {
+	public static String[] SIMULATION_CLIENT_TAGS = {DistributedTags.CLIENT, DistributedTags.GIPC, Comp533Tags.CUSTOM_IPC};
+	public static String[] SIMULATION_SERVER_TAGS = {DistributedTags.SERVER, DistributedTags.GIPC, Comp533Tags.CUSTOM_IPC};
+	public static String[] SIMULATION_REGISTRY_TAGS = {DistributedTags.REGISTRY, DistributedTags.GIPC, Comp533Tags.CUSTOM_IPC};
+
 	public Assignment4Requirements() {
 		Comp533TraceUtility.setTurnOn(true);
 		Comp533TraceUtility.setTracing();
 		GraderTraceUtility.setTurnOn(true);
 		GraderTraceUtility.setTracing();
 		
+		BasicProjectIntrospection.setCheckAllSpecifiedTags(true);
+		
 		addDueDate("02/4/2014 23:55:59", 1.0);
 		addDueDate("02/5/2014 23:55:59", 0.5);
 		
-		addFeature("Server tagged", 5, new SingleClassTaggedTestCase("Server"));
-		addFeature("Client tagged", 5, new SingleClassTaggedTestCase("Client"));
-		addFeature("Registry tagged", 5, new SingleClassTaggedTestCase("Registry"));
+		addFeature("Explicit Receive Server Tagged", 5, new SingleClassTagListTestCase(Comp533Tags.EXPLICIT_RECEIVE_SERVER));
+		addFeature("Explicit Receive Client1 Tagged", 5, new SingleClassTagListTestCase(Comp533Tags.EXPLICIT_RECEIVE_CLIENT1));
+		addFeature("Explicit Receive Client2 Tagged", 5, new SingleClassTagListTestCase(Comp533Tags.EXPLICIT_RECEIVE_CLIENT2));
+		
+		addFeature("Custom RPC Server Tagged", 5, new SingleClassTagListTestCase(Comp533Tags.CUSTOM_RPC_SERVER));
+		addFeature("Blocking Receive Client1 Tagged", 5, new SingleClassTagListTestCase(Comp533Tags.CUSTOM_RPC_CLIENT1));
+		addFeature("Blocking Receive Client2 Tagged", 5, new SingleClassTagListTestCase(Comp533Tags.CUSTOM_RPC_CLIENT2));
+		
+		addFeature("Blocking RPC Server Tagged", 5, new SingleClassTagListTestCase(Comp533Tags.BLOCKING_RPC_SERVER));
+		addFeature("Blocking Receive Client1 Tagged", 5, new SingleClassTagListTestCase(Comp533Tags.BLOCKING_RPC_CLIENT1));
+		addFeature("Blocking Receive Client2 Tagged", 5, new SingleClassTagListTestCase(Comp533Tags.BLOCKING_RPC_CLIENT2));
+
+		addFeature("Simulation Registry Tagged", 5, new SingleClassTagListTestCase(false, SIMULATION_REGISTRY_TAGS));
+		addFeature("Simulation Server Tagged", 5, new SingleClassTagListTestCase(SIMULATION_SERVER_TAGS));
+		addFeature("Simulation Client Client Tagged", 5, new SingleClassTagListTestCase(SIMULATION_CLIENT_TAGS));
+
+//		addFeature("Registry tagged", 5, new SingleClassTagListTestCase("Registry"));
 		
 //		addJUnitTestSuite(Assignment3Suite.class);
 		
