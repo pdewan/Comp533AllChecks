40895eb079a7d93f4650bedec8d01deee62426bd
Jedidiah Huang <jedshuang@gmail.com>
2020-02-17 17:13:28 -0500

    Changed A2 Requirements to allow rerun of infinite processes

--- src/gradingTools/comp533s20/assignment2/Assignment2Requirements.java
+++ src/gradingTools/comp533s20/assignment2/Assignment2Requirements.java
@@ -1,8 +1,10 @@
 package gradingTools.comp533s20.assignment2;
 
+import grader.basics.execution.BasicProjectExecution;
 import grader.junit.AJUnitProjectRequirements;
 import grader.trace.GraderTraceUtility;
 import grader.trace.comp533.Comp533TraceUtility;
+import gradingTools.comp533s19.assignment1.Assignment1Suite;
 import gradingTools.comp533s19.assignment1.testcases.OneClientConnection;
 import gradingTools.comp533s19.assignment1.testcases.OneClientReadWrite;
 import gradingTools.comp533s19.assignment1.testcases.OneClientCorrectThreadsTestCase;
@@ -12,11 +14,18 @@ import gradingTools.comp533s19.assignment1.testcases.SingleClassTaggedTestCase;
 import gradingTools.comp533s19.assignment1.testcases.StaticArguments;
 import gradingTools.comp533s19.assignment1.testcases.TwoClientConnection;
 import gradingTools.comp533s19.assignment1.testcases.TwoClientReadWrite;
+import trace.grader.basics.GraderBasicsTraceUtility;
+import util.trace.Tracer;
 import gradingTools.comp533s19.assignment1.testcases.TwoClientCorrectThreadsTestCase;
 import gradingTools.comp533s19.assignment1.testcases.TwoClientMessageRatioTestCase;
 
 public class Assignment2Requirements extends AJUnitProjectRequirements {
 	public Assignment2Requirements() {
+		
+		BasicProjectExecution.setReRunInfiniteProcesses(true);
+//		GraderBasicsTraceUtility.setBufferTracedMessages(false);
+//		Tracer.setMaxTraces(4000);
+		
 		Comp533TraceUtility.setTurnOn(true);
 		Comp533TraceUtility.setTracing();
 		GraderTraceUtility.setTurnOn(true);
