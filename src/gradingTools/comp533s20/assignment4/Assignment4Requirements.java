f3595ecc19ff5c8c42c6baf61fe023ba43c216b8
Jedidiah Huang <jedshuang@gmail.com>
2020-04-10 11:30:35 -0400

    A4 Requirements added

---
+++ src/gradingTools/comp533s20/assignment4/Assignment4Requirements.java
@@ -0,0 +1,45 @@
+package gradingTools.comp533s20.assignment4;
+
+import grader.basics.execution.BasicProjectExecution;
+import grader.junit.AJUnitProjectRequirements;
+import grader.trace.GraderTraceUtility;
+import grader.trace.comp533.Comp533TraceUtility;
+import gradingTools.comp533s19.assignment1.Assignment1Suite;
+import gradingTools.comp533s19.assignment1.testcases.OneClientConnection;
+import gradingTools.comp533s19.assignment1.testcases.OneClientReadWrite;
+import gradingTools.comp533s19.assignment1.testcases.OneClientCorrectThreadsTestCase;
+import gradingTools.comp533s19.assignment1.testcases.OneClientMessageRatioTestCase;
+import gradingTools.comp533s19.assignment1.testcases.ReadWriteUpdateOrderTestCase;
+import gradingTools.comp533s19.assignment1.testcases.SingleClassTaggedTestCase;
+import gradingTools.comp533s19.assignment1.testcases.StaticArguments;
+import gradingTools.comp533s19.assignment1.testcases.TwoClientConnection;
+import gradingTools.comp533s19.assignment1.testcases.TwoClientReadWrite;
+import trace.grader.basics.GraderBasicsTraceUtility;
+import util.trace.Tracer;
+import gradingTools.comp533s19.assignment1.testcases.TwoClientCorrectThreadsTestCase;
+import gradingTools.comp533s19.assignment1.testcases.TwoClientMessageRatioTestCase;
+
+public class Assignment4Requirements extends AJUnitProjectRequirements {
+	public Assignment4Requirements() {
+		
+//		BasicProjectExecution.setReRunInfiniteProcesses(true);
+//		GraderBasicsTraceUtility.setBufferTracedMessages(false);
+//		Tracer.setMaxTraces(4000);
+		
+		Comp533TraceUtility.setTurnOn(true);
+		Comp533TraceUtility.setTracing();
+		GraderTraceUtility.setTurnOn(true);
+		GraderTraceUtility.setTracing();
+		addDueDate("03/25/2020 01:00:00", 1.05); // early extra
+		//addDueDate("02/21/2020 01:00:00", 1.03); // other early extra
+		addDueDate("03/27/2020 01:00:00", 1.0); // regular
+		addDueDate("04/3/2020 01:00:00", 0.90); // one week late
+		addDueDate("04/10/2020 01:00:00", 0.75); // two weeks late
+		
+		addJUnitTestSuite(Assignment4Suite.class);
+        addManualFeature("Test Cases", 10, false);
+        addManualFeature("Experimental Input", 10, false);
+		
+
+	}
+}
