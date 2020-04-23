8c7d8d25423b7c721386149f237749fcaeb666c1
Jedidiah Huang <jedshuang@gmail.com>
2020-04-23 19:14:24 -0400

    Updated A6 Due Date and Added A7 Requirements

--- src/gradingTools/comp533s20/assignment7/Assignment7Requirements.java
+++ src/gradingTools/comp533s20/assignment7/Assignment7Requirements.java
@@ -1,4 +1,4 @@
-package gradingTools.comp533s20.assignment6;
+package gradingTools.comp533s20.assignment7;
 
 import grader.basics.execution.BasicProjectExecution;
 import grader.config.ExecutionSpecificationSelector;
@@ -20,8 +20,8 @@ import util.trace.Tracer;
 import gradingTools.comp533s19.assignment1.testcases.TwoClientCorrectThreadsTestCase;
 import gradingTools.comp533s19.assignment1.testcases.TwoClientMessageRatioTestCase;
 
-public class Assignment6Requirements extends AJUnitProjectRequirements {
-	public Assignment6Requirements() {
+public class Assignment7Requirements extends AJUnitProjectRequirements {
+	public Assignment7Requirements() {
 		
 //		BasicProjectExecution.setReRunInfiniteProcesses(true);
 //		GraderBasicsTraceUtility.setBufferTracedMessages(false);
@@ -32,14 +32,15 @@ public class Assignment6Requirements extends AJUnitProjectRequirements {
 		Comp533TraceUtility.setTracing();
 		GraderTraceUtility.setTurnOn(true);
 		GraderTraceUtility.setTracing();
-		addDueDate("04/10/2020 01:00:00", 1.05); // early extra
-		addDueDate("04/15/2020 01:00:00", 1.03); // other early extra
-		addDueDate("04/17/2020 01:00:00", 1.0); // regular
-		addDueDate("04/25/2020 01:00:00", 0.90); // one week late
+		addDueDate("04/22/2020 01:00:00", 1.05); // early extra
+		addDueDate("04/24/2020 01:00:00", 1.03); // other early extra
+		addDueDate("04/25/2020 01:00:00", 1.0); // regular
+		addDueDate("04/27/2020 01:00:00", 0.90); // one week late
 		//addDueDate("04/20/2020 01:00:00", 0.75); // two weeks late
 		
-		addJUnitTestSuite(Assignment6Suite.class);
-        addManualFeature("Test Cases", 10, false);
-        addManualFeature("Experimental Input", 10, false);
+		addJUnitTestSuite(Assignment7Suite.class);
+        addManualFeature("Atomic Test Cases", 10, false);
+        addManualFeature("Experimental Input", 15, false);
+        addManualFeature("Consensus Test Cases", 10, true);
 	}
 }
